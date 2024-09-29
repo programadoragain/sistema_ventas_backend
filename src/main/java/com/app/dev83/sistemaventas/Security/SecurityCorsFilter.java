ed0ee32cd647718e7b5453470e68c5d17660e406
programadoragain <ferdandi04@gmail.com>
2024-09-29 20:43:37 -0300

    Cambios en clases de seguridad y cors. Se agrego la clase Gastos Se agrego el controlador Dashboard y se le dio funcionalidad

---
+++ src/main/java/com/app/dev83/sistemaventas/Security/SecurityCorsFilter.java
@@ -0,0 +1,28 @@
+package com.app.dev83.sistemaventas.Security;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsFilter {
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true); // Si necesitas permitir credenciales
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
